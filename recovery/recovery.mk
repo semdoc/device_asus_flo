c6c819aa999c7fb3fbc5fa5af8bfda650e991363
Thunder07 <m.a.hot.m+git@gmail.com>
2015-05-04 18:19:40 +0300

    MR & TWRP config & set lzma as default compression
    Change-Id: Id754135e7ba91877e5ee7386e0e1f1c60c4907cb

--- recovery/recovery.mk
+++ recovery/recovery.mk
@@ -1,24 +1,30 @@
-TF_BLOBIFIER := $(HOST_OUT_EXECUTABLES)/blobpack_tfp
-LZMA_BIN := /usr/bin/lzma
+LZMA := /usr/bin/lzma
 
 
+$(recovery_uncompressed_ramdisk): $(MINIGZIP) \
+	$(TARGET_RECOVERY_ROOT_TIMESTAMP)
+	@echo -e ${PRT_IMG}"----- Making uncompressed recovery ramdisk ------"${CL_RST}
+	$(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $@
+
+$(recovery_ramdisk): $(MKBOOTFS) \
+	$(recovery_uncompressed_ramdisk)
+	@echo -e ${CL_CYN}"----- Making recovery ramdisk ------"${CL_RST}
+ifndef BOARD_LZMA_RECOVERY_RD
+	$(MINIGZIP) < $(recovery_uncompressed_ramdisk) > $@
+else
+	$(LZMA) < $(recovery_uncompressed_ramdisk) > $@
+endif
+
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
+	$(call pretty,"Target boot image: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
+	
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) \
-		$(TF_BLOBIFIER) \
 		$(recovery_kernel)
-	@echo ----- Compressing recovery ramdisk with lzma ------
-	rm -f $(recovery_uncompressed_ramdisk).lzma
-	$(LZMA_BIN) $(recovery_uncompressed_ramdisk)
-	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
-	@echo ----- Making recovery image ------
-	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@.orig
-	$(TF_BLOBIFIER) $@ SOS $@.orig
-	@echo ----- Made recovery image -------- $@
+	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
-
-
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(TF_BLOBIFIER)
-	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) --output $@.orig
-	$(TF_BLOBIFIER) $@ LNX $@.orig
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
