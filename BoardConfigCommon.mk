f948ffa2f905cea959dfc071f7af699511f9d0f2
Matt Mower <mowerm@gmail.com>
2016-02-07 20:18:44 +0200

    flo: Add "twrp=" to flags in fstab and cleanup
    Change-Id: Id7367089638ca7702302836eba5eceaaa6966d6f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,18 +134,12 @@ BOARD_HARDWARE_CLASS += hardware/cyanogen
 
 #TWRP config:
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-DEVICE_RESOLUTION := 1200x1920
-TW_THEME := portrait_hdpi
 TW_INCLUDE_CRYPTO := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_NO_USB_STORAGE := true
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
 TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
 TW_CUSTOM_POWER_BUTTON := 107
-TW_BRIGHTNESS_PATH := /sys/devices/platform/msm_fb.591617/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 255
-TW_NO_SCREEN_BLANK := true
 
 -include vendor/asus/flo/BoardConfigVendor.mk
